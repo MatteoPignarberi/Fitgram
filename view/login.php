cde15db9693232499532df808464466d1c7f2c01
Matteo Pignarberi <pignarberi.matteo.stud@itisgalileiroma.it>
2026-04-11 11:45:32 +0200

    modifica view 2.0

--- view/login.php
+++ view/login.php
@@ -7,32 +7,33 @@ session_start();
     <meta charset="UTF-8">
     <title>Login Fitgram</title>
     <link rel="stylesheet" href="../styles/css/login.css">
-    <link rel="stylesheet" href="../styles/css/components.css">
 </head>
 <body>
 
-<?php if (isset($_SESSION['errore_login'])): ?>
-    <div style="color: #721c24; background-color: #f8d7da; padding: 10px; border-radius: 4px; border: 1px solid #f5c6cb; margin-bottom: 15px;">
-        <?php
-        echo $_SESSION['errore_login'];
-        unset($_SESSION['errore_login']);
-        ?>
-    </div>
-<?php endif; ?>
+<div class="container">
 
-<form id="form-login" action="../controller/loginController.php" method="POST">
+    <h1>Login</h1>
 
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
-    <br><br>
+    <?php if (isset($_SESSION['errore_login'])): ?>
+        <div style="color: #721c24; background-color: #f8d7da; padding: 10px; border-radius: 4px; border: 1px solid #f5c6cb; margin-bottom: 15px; font-size: 14px;">
+            <?php
+            echo $_SESSION['errore_login'];
+            unset($_SESSION['errore_login']);
+            ?>
+        </div>
+    <?php endif; ?>
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required>
-    <br><br>
+    <form id="form-login" action="../controller/loginController.php" method="POST">
 
-    <button type="submit">Accedi</button>
+        <input type="text" id="username" name="username" placeholder="Username" required>
 
-</form>
+        <input type="password" id="password" name="password" placeholder="Password" required>
+
+        <input type="submit" value="Accedi">
+
+    </form>
+
+</div>
 
 </body>
 </html>
